e461e9bcc627abf449521267789c6746ba47e5d9
Alexander Dupree <adupree@ada.cs.pdx.edu>
2020-01-14 00:10:19 -0800

    Added time uprog

---
+++ xv6-pdx/time.c
@@ -0,0 +1,17 @@
+/*
+ * File         : ps.c
+ * Description  : User program to display process information
+ * Author       : Alexander DuPree
+ * Date         : 13 Jan 2019
+*/
+
+#include "types.h"
+#include "user.h"
+
+int
+main(int argc, char* argv[])
+{
+    printf(1, "Time called\n");
+
+    exit();
+}
