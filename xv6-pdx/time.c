605c04ee85ae2223f101227dc80e17a2d05bf289
Alexander Dupree <adupree@ada.cs.pdx.edu>
2020-01-14 13:20:04 -0800

    Finished time user program

--- xv6-pdx/time.c
+++ xv6-pdx/time.c
@@ -1,6 +1,6 @@
 /*
- * File         : ps.c
- * Description  : User program to display process information
+ * File         : time.c
+ * Description  : User program to time process execution
  * Author       : Alexander DuPree
  * Date         : 13 Jan 2019
 */
@@ -11,7 +11,32 @@
 int
 main(int argc, char* argv[])
 {
-    printf(1, "Time called\n");
+    int start_ticks = uptime();
+    int pid = fork();
 
+    if(pid < 0)
+    {
+        printf(2, "TIME: Fork error occured. Exiting.");
+    }
+    else if(pid == 0) // Child
+    {
+        // Execute command, no need to check RC.
+        exec(argv[1], argv + 1);
+    }
+    else
+    {
+        if(wait() < 0) 
+        { 
+            printf(2, "TIME: Wait error occured");
+        }
+        else
+        {
+            int elapsed = uptime() - start_ticks;
+            int seconds = elapsed / 1000;
+            int ms      = elapsed % 1000;
+
+            printf(1, "%s ran in %d.%ds\n", argv[1], seconds, ms);
+        }
+    }
     exit();
 }
