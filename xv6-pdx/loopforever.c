3d9f095103be58384a96092e09696bd3dec6df5b
Alexander Dupree <adupree@ada.cs.pdx.edu>
2020-02-11 22:01:47 -0800

    Added more options to loopforever.c

--- xv6-pdx/loopforever.c
+++ xv6-pdx/loopforever.c
@@ -10,6 +10,27 @@
 #include "user.h"
 #include "pdx.h"
 
+int fork_process()
+{
+  int pid = fork();
+  if(pid < 0) 
+  {
+    printf(2, "loopforever: Fork error occured. Exiting.");
+    exit();
+  }
+  return pid;
+}
+
+void wait_for_child()
+{
+  if(wait() < 0)
+  {
+    printf(2, "loopforever: Wait error occured");
+    exit();
+  }
+  return;
+}
+
 static void
 help()
 {
@@ -26,33 +47,60 @@ loopforever()
   }
 }
 
+static void
+loop()
+{
+  int i = 0; 
+  while(i < ~0)
+  {
+    ++i;
+  }
+  exit();
+}
+
 int
 main(int argc, char *argv[])
 {
-  if(argc != 2)
+  if(argc < 2)
   {
     help();
     exit();
   }
 
+  int wait = 0;
+  int child_exit = 0;
   int children = atoi(argv[1]) - 1;
 
+  if(argc >= 3 && strncmp(argv[2], "--wait", 6) == 0)
+  {
+    wait = 1;
+  }
+  if(argc >= 3 && strncmp(argv[2], "--child_exit", 12) == 0)
+  {
+    child_exit = 1;
+  }
 
   for(int i = 0; i < children; ++i)
   {
-    int pid = fork();
-    if(pid == -1)
-    {
-      printf(2, "\nloopforever: ERROR, failed to create process");
-      exit();
-    }
+    int pid = fork_process();
     if(pid == 0)
     {
-      loopforever();
+      (child_exit) ? loop() : loopforever();
     }
   }
-  printf(1, "\nCreated %d children, starting loopforever on parent\n", children);
+  printf(1, "\nCreated %d children.", children);
+  if(wait)
+  {
+    printf(1, " Kill children to wakeup parent\n", children);
+    while(children)
+    {
+      wait_for_child();
+      --children;
+    }
+    printf(1, "\nParent Awake! Finished waiting for children!");
+  }
+  printf(1, " Starting loopforever on parent\n");
   loopforever();
 
-  exit(); // Never gets called
+  exit();
 }
